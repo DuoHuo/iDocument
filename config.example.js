23dc7a451b072a17112593009aa36269549b46f4
blueed <blueed3@gmail.com>
2014-02-27 21:51:25 +0800

    search form

---
+++ config.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // server settings
+    cookieSecret: 'YourCookieSecretHere',
+    mongodb: 'mongodb://127.0.0.1:27017/dhdocs-dev',
+    bindAddress: '127.0.0.1',
+
+    // site settings
+    siteName: 'iDocument'
+}
--- dhdocs.js
+++ dhdocs.js
@@ -12,7 +12,7 @@ var config = require('./config.js');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', config.bindPort || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
