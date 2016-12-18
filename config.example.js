c1d37db5b7c1e2e9304829f87c75a59c2ee05d8d
Blacate <244787126y@gmail.com>
2016-12-18 18:33:41 +0800

    add function convert to pdf

--- config.example.js
+++ config.example.js
@@ -1,17 +1,18 @@
 module.exports = {
-  // server settings
-  host: '127.0.0.1:3000',
-  cookieSecret: 'naev5aeSthooceN6Ahhu1een',
-  mongodb: 'mongodb://127.0.0.1:27017/dhdocs',
-  bindAddress: '127.0.0.1',
-  bindPort: 3000,
+    // server settings
+    host: '127.0.0.1:3000',
+    cookieSecret: 'naev5aeSthooceN6Ahhu1een',
+    mongodb: 'mongodb://127.0.0.1:27017/dhdocs',
+    bindAddress: '127.0.0.1',
+    bindPort: 3000,
 
-  // site settings
-  siteName: 'iDocument',
-  qiniu: {
-  	bucket: 'idoc',
-    hostname: 'http://7xv7vu.com1.z0.glb.clouddn.com',
-  	access_key: 'srpwkjJuIfB-_GYv5yryMccW46MmmyhTugNhLPQK',
-  	secret_key: 'FvY-zarqGAL6fy95kNn4xUGpmxELQdCotNpMkzIg'
-  }
+    // site settings
+    siteName: 'iDocument',
+    qiniu: {
+        bucket: 'idoc',
+        hostname: 'http://7xv7vu.com1.z0.glb.clouddn.com',
+        pdf: 'dtZmaCn4XGRl-DcpTVcYhNsMXWE=',
+        access_key: 'srpwkjJuIfB-_GYv5yryMccW46MmmyhTugNhLPQK',
+        secret_key: 'FvY-zarqGAL6fy95kNn4xUGpmxELQdCotNpMkzIg'
+    }
 }
--- controllers/media.js
+++ controllers/media.js
@@ -10,117 +10,118 @@ const imageTypes = ["image/jpeg", "image/png"];
 const fileTypes = ["application/pdf", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/msword"];
 
 exports.getAll = function(req, res) {
-  var type = req.query.type || 'doc';
+    var type = req.query.type || 'doc';
 
-  MediaService.getAll(type).then(function(medias) {
-    res.send(200, medias);
-  }).catch(function(err) {
-    res.send(400, err);
-  });
+    MediaService.getAll(type).then(function(medias) {
+        res.send(200, medias);
+    }).catch(function(err) {
+        res.send(400, err);
+    });
 };
 
-exports.findDocByName = function(req, res){
-  var q = req.query.q;
+exports.findDocByName = function(req, res) {
+    var q = req.query.q;
 
-  MediaService.search(q)
-  .then(function(docs){
-    res.send(200, docs);
-  })
-  .catch(function(err){
-    res.send(400, err);
-  })
+    MediaService.search(q)
+        .then(function(docs) {
+            res.send(200, docs);
+        })
+        .catch(function(err) {
+            res.send(400, err);
+        })
 };
 
 exports.delete = function(req, res) {
-  var id = req.params.mediaId;
-  if (!id) {
-    res.send(405, 'method is not allowed');
-    return;
-  }
+    var id = req.params.mediaId;
+    if (!id) {
+        res.send(405, 'method is not allowed');
+        return;
+    }
 
-  MediaService.delete(id).then(function() {
-    res.send(204);
-  }).catch(function(err) {
-    res.send(400, err);
-  });
+    MediaService.delete(id).then(function() {
+        res.send(204);
+    }).catch(function(err) {
+        res.send(400, err);
+    });
 };
 
 exports.upload = function(req, res) {
-  var TEMP_PATH = req.file.path;
-  var TARGET_PATH = 'public/upload/' + req.file.originalname;
-  var URL_PATH = '/upload/' + req.file.originalname;
-  var mimetype;
+    var TEMP_PATH = req.file.path;
+    var TARGET_PATH = 'public/upload/' + req.file.originalname;
+    var URL_PATH = '/upload/' + req.file.originalname;
+    var mimetype;
 
-  if(~imageTypes.indexOf(req.file.mimetype)) {
-    mimetype = 'image';
-  } else if (~fileTypes.indexOf(req.file.mimetype)) {
-    mimetype = 'doc';
-  } else {
-    res.send(400, {msg: '只能上传png,jpg格式的图片或者doc, docx, pdf格式的文档'});
-  };
+    if (~imageTypes.indexOf(req.file.mimetype)) {
+        mimetype = 'image';
+    } else if (~fileTypes.indexOf(req.file.mimetype)) {
+        mimetype = 'doc';
+    } else {
+        res.send(400, { msg: '只能上传png,jpg格式的图片或者doc, docx, pdf格式的文档' });
+    };
 
-  fs.rename(TEMP_PATH, TARGET_PATH, function(err) {
-    if(err) console.log(err);
-    Util.uploadFile(req.file.originalname, TARGET_PATH)
-    .then(function(reply) {
-        // 上传成功
-      var media = {
-        key: req.file.originalname,
-        local_url: config.host + URL_PATH,
-        type: mimetype,
-        qiniu_url: qiniuConfig.hostname + '/' + reply.key,
-        hash: reply.hash
-      };
+    fs.rename(TEMP_PATH, TARGET_PATH, function(err) {
+        if (err) console.log(err);
+        Util.uploadAndtopdf(req.file.originalname, TARGET_PATH)
+            .then(function(reply) {
+                // 上传成功
+                var media = {
+                    key: req.file.originalname,
+                    local_url: config.host + URL_PATH,
+                    type: mimetype,
+                    qiniu_url: qiniuConfig.hostname + '/' + qiniuConfig.pdf + '/' + reply.hash,
+                    hash: reply.hash
+                };
 
-      MediaService.create(media).then(function(media) {
-        res.json(media);
-      })
-    }).catch(function(err) {
-      res.send(400, err);
-    });
-  })
+                MediaService.create(media).then(function(media) {
+                    res.json(media);
+                })
+            }).catch(function(err) {
+                console.log(err);
+                res.send(400, { msg: err });
+            });
+    })
 };
 
 exports.batchUpload = function(req, res) {
-  var files = req.files;
+    var files = req.files;
 
-  function resolveImg(file) {
-    var TEMP_PATH = file.path;
-    var TARGET_PATH = 'public/upload/' + file.originalname;
-    var URL_PATH = '/upload/' + file.originalname;
-    var mimetype;
+    function resolveImg(file) {
+        var TEMP_PATH = file.path;
+        var TARGET_PATH = 'public/upload/' + file.originalname;
+        var URL_PATH = '/upload/' + file.originalname;
+        var mimetype;
 
-    if(~imageTypes.indexOf(file.mimetype)) {
-      mimetype = 'image';
-    } else if (~fileTypes.indexOf(file.mimetype)) {
-      mimetype = 'doc';
-    } else {
-      throw new Error('只能上传png,jpg格式的图片或者doc, docx, pdf格式的文档');
-    };
+        if (~imageTypes.indexOf(file.mimetype)) {
+            mimetype = 'image';
+        } else if (~fileTypes.indexOf(file.mimetype)) {
+            mimetype = 'doc';
+        } else {
+            throw new Error('只能上传png,jpg格式的图片或者doc, docx, pdf格式的文档');
+        };
 
-    fs.renameSync(TEMP_PATH, TARGET_PATH);
-    return Util.uploadFile(file.originalname, TARGET_PATH)
-    .then(function(reply) {
-      // 上传成功
-      var media = {
-        key: file.originalname,
-        local_url: config.host + URL_PATH,
-        type: mimetype,
-        qiniu_url: qiniuConfig.hostname +  '/' + reply.key,
-        hash: reply.hash
-      };
-      return MediaService.create(media);
-    })
-  }
+        fs.renameSync(TEMP_PATH, TARGET_PATH);
+        return Util.uploadFile(file.originalname, TARGET_PATH)
+            .then(function(reply) {
+                // 上传成功
+                var media = {
+                    key: file.originalname,
+                    local_url: config.host + URL_PATH,
+                    type: mimetype,
+                    qiniu_url: qiniuConfig.hostname + '/' + reply.key,
+                    hash: reply.hash
+                };
+                return MediaService.create(media);
+            })
+    }
 
-  var promises = files.map(function(item) {
-    return resolveImg(item);
-  });
+    var promises = files.map(function(item) {
+        return resolveImg(item);
+    });
 
-  Promise.all(promises).then(function(results) {
-    res.json(results);
-  })
-  .catch(function(err) {
-    res.send(400, err);
-  });
+    Promise.all(promises).then(function(results) {
+            res.json(results);
+        })
+        .catch(function(err) {
+            res.send(400, err);
+        });
 };
